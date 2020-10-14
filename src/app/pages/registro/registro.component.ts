2d66474a28cc16b50c0d7eac6fb1daefb89d3335
David Egea Lozano <david.egea.daw@gmail.com>
2020-10-14 21:51:17 +0200

    Validaciones

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -6,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  @Input()
+  usuario : UsuarioModel;
 
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
 
+   }
 
+  onSubmit( form: NgForm){
+
+    if(form.invalid){return ;}
+    console.log('se ha pulsado en crear cuenta')
+    console.log(this.usuario);
+    console.log(form);
+  }
 }
